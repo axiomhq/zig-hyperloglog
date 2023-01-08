be1c50aa14a5fadef5286f4ead73c32c1039ef86
Seif Lotfy <seif.lotfy@gmail.com>
2023-01-08 13:42:03 +0100

    add initial sparse

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,8 @@ const math = std.math;
 const testing = std.testing;
 const Allocator = std.mem.Allocator;
 const RndGen = std.rand.DefaultPrng;
+const sparse = @import("sparse.zig");
+const AllocationError = error{OutOfMemory};
 
 const precision = 14;
 const m = 1 << precision;
@@ -26,50 +28,91 @@ fn beta(z: f64) f64 {
 
 const HyperLogLog = struct {
     const Self = @This();
-    data: []u8,
+
     allocator: Allocator,
+    dense: []u8,
+    is_sparse: bool = true,
+    set: sparse.Set,
+    counter: u64 = 0,
 
     pub fn init(allocator: Allocator) !Self {
-        var data = allocator.alloc(u8, m) catch |err| switch (err) {
-            error.OutOfMemory => return error.OutOfMemory,
-        };
-        for (data) |*x| {
-            x.* = 0;
-        }
-        return Self{ .data = data, .allocator = allocator };
+        return Self{ .allocator = allocator, .dense = try allocator.alloc(u8, 0), .set = sparse.Set.init(allocator) };
     }
 
     pub fn deinit(self: *Self) void {
-        self.allocator.free(self.data);
+        if (self.is_sparse) {
+            self.set.deinit();
+        }
+        self.allocator.free(self.dense);
     }
 
-    pub fn addHashed(self: *Self, hash: u64) void {
-        var k = hash >> max;
-        var val = @intCast(u8, @clz((hash << precision) ^ maxX)) + 1;
-        if (val > self.data[k]) {
-            self.data[k] = val;
+    // return void or error if out of memory
+    pub fn toDense(self: *Self) !void {
+        std.debug.print("switching to dense, len: {}\n", .{self.set.len()});
+        self.dense = self.allocator.alloc(u8, m) catch |err| switch (err) {
+            error.OutOfMemory => return error.OutOfMemory,
+        };
+        for (self.dense) |*x| {
+            x.* = 0;
+        }
+        if (self.is_sparse) {
+            var itr = self.set.set.iterator();
+
+            while (itr.next()) |x| {
+                var val: u64 = x.key_ptr.*;
+                try self.addToDense(val);
+            }
+            self.set.deinit();
+            self.is_sparse = false;
         }
     }
 
-    pub fn cardinality(self: *Self) f64 {
+    fn addToSparse(self: *Self, hash: u64) !void {
+        try self.set.add(hash);
+    }
+
+    fn addToDense(self: *Self, x: u64) !void {
+        var k = x >> max;
+        var val = @intCast(u8, @clz((x << precision) ^ maxX)) + 1;
+        if (val > self.dense[k]) {
+            self.dense[k] = val;
+        }
+    }
+
+    pub fn addHashed(self: *Self, hash: u64) !void {
+        self.counter += 1;
+        if (self.is_sparse == true and self.set.len() * 4 < m) {
+            return self.addToSparse(hash);
+        }
+        if (self.is_sparse == true) {
+            try self.toDense();
+        }
+        try self.addToDense(hash);
+    }
+
+    pub fn cardinality(self: *Self) u64 {
+        if (self.is_sparse) {
+            return @intCast(u64, self.set.len());
+        }
+
         var sum: f64 = 0;
         var z: f64 = 0;
-        for (self.data) |x| {
+        for (self.dense) |x| {
             if (x == 0) {
                 z += 1;
             }
             sum += 1.0 / math.pow(f64, 2.0, @intToFloat(f64, x));
         }
-        const m_float = @intToFloat(f64, @intCast(u64, self.data.len));
+        const m_float = @intToFloat(f64, m);
 
         var est = alpha * m_float * (m_float - z) / (beta(z) + sum);
-        return est;
+        return @floatToInt(u64, est + 0.5);
     }
 
     pub fn merge(self: *Self, other: *Self) void {
-        for (self.data) |*x, i| {
-            if (other.data[i] > x.*) {
-                x.* = other.data[i];
+        for (self.dense) |*x, i| {
+            if (other.dense[i] > x.*) {
+                x.* = other.dense[i];
             }
         }
     }
@@ -77,26 +120,20 @@ const HyperLogLog = struct {
 
 test "init" {
     var hll = HyperLogLog.init(testing.allocator) catch unreachable;
-    testing.expect(hll.data.len == m) catch unreachable;
+    defer hll.deinit();
 
-    const x = 3 << 50;
+    var i: u64 = 0;
+    while (i < 20000) : (i += 1) {
+        // hash i and add it to the hll
+        var s = std.fmt.allocPrint(testing.allocator, "{}", .{i}) catch unreachable;
+        defer testing.allocator.free(s);
+        var hash = std.hash.CityHash64.hash(s);
 
-    hll.addHashed(x);
-
-    // add a bunch of hashes
-    var i: u32 = 1;
-    while (i <= 10000000) : (i += 1) {
-        // create random u64
-        const r = rnd.random().int(u64);
-        hll.addHashed(r);
+        try hll.addHashed(hash);
     }
 
-    const est = hll.cardinality();
-
-    // print the estimate
-    std.debug.print("estimate: {}\n", .{est});
-
-    testing.expect(est >= 1000) catch unreachable;
-
-    defer hll.deinit();
+    // print cardinality
+    std.debug.print("cardinality: {}\n", .{hll.cardinality()});
+    std.debug.print("counter: {}\n", .{hll.counter});
+    //try testing.expect(hll.cardinality() == 1);
 }
