496574d512f89792a515f233d7e2e4322affe2ad
Seif Lotfy <seif.lotfy@gmail.com>
2023-01-09 17:24:09 +0100

    add HyperLogLogDefault

---
+++ src/example.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+const HyperLogLog = @import("main.zig").DefaultHyperLogLog;
+const RndGen = std.rand.DefaultPrng;
+
+var rnd = RndGen.init(0);
+
+pub fn main() !void {
+    const count = 1e7;
+    const alloc = std.heap.page_allocator;
+
+    var hll = try HyperLogLog().init(alloc);
+    defer hll.deinit();
+
+    var i: u64 = 0;
+    while (i < count) : (i += 1) {
+        const x = rnd.random().int(u64);
+        try hll.add_hashed(x);
+    }
+
+    const est = hll.cardinality();
+    std.debug.print("Estimated cardinality: {d}\n", .{est});
+}
